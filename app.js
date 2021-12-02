dc582e6cf6cdcaf7be0662174e7d8d4c59976d79
Alexandre Roberto <alexandreroberto.94@gmail.com>
2021-12-01 20:09:46 -0400

    Instalando EJS e Começando primeira página

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require("express");
+const path = require("path");
+
 const checkListRouter = require("./src/routes/checklist");
-require('./config/database')
+const rootRouter = require("./src/routes/index");
+
+require("./config/database");
 
 const app = express();
 app.use(express.json());
 
+//usar arquivos estaticos
+app.use(express.static(path.join(__dirname, "public")));
+
+//carregar views e instalar engine
+app.set("views", path.join(__dirname, "src/views"));
+app.set("view engine", "ejs");
+
+//importando root e checklist como middleware
+app.use("/", rootRouter);
 app.use("/checklists", checkListRouter);
 
 app.listen(3000, () => {
